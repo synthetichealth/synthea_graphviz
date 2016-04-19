1104b721757ac030d3701102fb03e6797ddccf3f
Jason Walonoski <jwalonoski@mitre.org>
2016-04-19 09:03:29 -0400

    Color the nodes of the graph by module.

--- synthea_graphviz.rb
+++ synthea_graphviz.rb
@@ -3,24 +3,30 @@
 require 'graphviz'
 require 'synthea'
 
+# color (true) or black & white (false)
+COLOR = true
+
 # Create a new graph
 g = GraphViz.new( :G, :type => :digraph )
 
-# # Create two nodes
-# hello = g.add_nodes( "Hello" )
-# world = g.add_nodes( "World" )
-
-# # Create an edge between the two nodes
-# g.add_edges( hello, world )
-
 # Create the list of items
 items = []
+modules = {}
 Synthea::Rules.metadata.each do |key,rule|
   items << key
   items << rule[:inputs]
   items << rule[:outputs]
+  modules[ rule[:module_name] ] = true
 end
 items = items.flatten.uniq
+
+# Choose a color for each module
+# available_colors = GraphViz::Utils::Colors::COLORS.keys
+available_colors = ['palevioletred','orange','lightgoldenrod','palegreen','lightblue','lavender','purple']
+modules.keys.each_with_index do |key,index|
+  modules[key] = available_colors[index]
+end
+attribute_color = 'grey'
 
 # Create a node for each item
 nodes = {}
@@ -28,13 +34,23 @@ items.each{|i|nodes[i]=g.add_node(i.to_s)}
 
 # Make items that are not rules boxes
 components = nodes.keys - Synthea::Rules.metadata.keys
-components.each{|i|nodes[i]['shape']='Box'}
+components.each do |i|
+  nodes[i]['shape']='Box'
+  if COLOR
+    nodes[i]['color']=attribute_color
+    nodes[i]['style']='filled'
+  end
+end
 
 # Create the edges
 edges = []
 
 Synthea::Rules.metadata.each do |key,rule|
   node = nodes[key]
+  if COLOR
+    node['color'] = modules[rule[:module_name]]
+    node['style'] = 'filled'
+  end
   begin
     rule[:inputs].each do |input|
       other = nodes[input]
